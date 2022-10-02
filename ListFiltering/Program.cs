824c9273ba6a85292ee80060b50fd0750896f678
PrgaTest123 <marintest0@gmail.com>
2022-10-02 22:56:12 +0200

    Add project files.

---
+++ ListFiltering/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace ListFiltering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> item = new List<object>() {1, 2, "a", "b"};
+            Console.WriteLine(string.Join(",", GetIntegersFromList(item)));
+        }
+
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            //Best practice
+            // return listOfItems.OfType<int>();
+            var result = new List<int>();
+            foreach (var item in listOfItems)
+            {
+                if (item is int)
+                    result.Add((int)item);
+            }
+            return result;
+        }
+
+
+    }
+}
